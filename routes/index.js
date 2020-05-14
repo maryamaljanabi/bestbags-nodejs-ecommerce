ee9d40ff9729e1a469ef774a0b7469d32552f290
Maryam <maria.i.135a@gmail.com>
2020-05-14 16:55:19 +0300

    Add home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const Product = require("../models/product");
+const Category = require("../models/category");
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", async (req, res) => {
-  res.render("shop/home", { pageName: "Home" });
+  try {
+    const products = await Product.find({})
+      .sort("-createdAt")
+      .populate("category");
+    res.render("shop/home", { pageName: "Home", products });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;

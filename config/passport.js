ae82e84f0ebb2094fc21c88f12d86d41e197b51d
maryamaljanabi <maryam.aljanabi135@gmail.com>
2020-05-15 00:56:30 +0300

    Add user signup and login

---
+++ config/passport.js
@@ -0,0 +1,67 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+passport.use(
+  "local.signup",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: false,
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email: email });
+        console.log(user);
+        if (user) {
+          return done(null, false, { message: "Email already exists" });
+        }
+        const newUser = await new User();
+        newUser.email = email;
+        newUser.password = newUser.encryptPassword(password);
+        await newUser.save();
+        return done(null, newUser);
+      } catch (error) {
+        console.log(error);
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  "local.signin",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: false,
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          return done(null, false, { message: "User doesn't exist" });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: "Wrong password" });
+        }
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+        return done(error);
+      }
+    }
+  )
+);

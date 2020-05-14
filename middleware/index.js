4b91a1d2cc5a3ddc94f57d05d81eab86aa1f5061
Maryam <maria.i.135a@gmail.com>
2020-05-15 00:56:30 +0300

    Add user signup and login

---
+++ middleware/index.js
@@ -0,0 +1,18 @@
+let middlewareObject = {};
+
+//a middleware to check if a user is logged in or not
+middlewareObject.isNotLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+};
+
+middlewareObject.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+};
+
+module.exports = middlewareObject;
---
+++ models/order.js
@@ -0,0 +1,63 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var orderSchema = Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  cart: {
+    totalQty: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    totalCost: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        qty: {
+          type: Number,
+          default: 0,
+        },
+        price: {
+          type: Number,
+          default: 0,
+        },
+        title: {
+          type: String,
+        },
+      },
+    ],
+  },
+  //checkout form info
+  address: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  paymentId: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  Delivered: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Order", orderSchema);

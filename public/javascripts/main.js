c1712c6600776cd76997720391449fc7a7c0b740
maryamaljanabi <maryam.aljanabi135@gmail.com>
2020-05-15 15:08:47 +0300

    Add shopping cart

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+// fade out for flash messages
+setTimeout(function () {
+  $("#flash-msg").fadeOut("slow");
+}, 3000);
+
+setTimeout(function () {
+  $("#success").fadeOut("slow");
+}, 3000);
+
+setTimeout(function () {
+  $("#error").fadeOut("slow");
+}, 3000);

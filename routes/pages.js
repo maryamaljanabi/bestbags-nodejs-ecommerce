0585aeb09866d4694b9a55906d01bcedd33d8b9d
maryamaljanabi <maryam.aljanabi135@gmail.com>
2020-05-17 22:06:01 +0300

    Add about us route and view

--- routes/pages.js
+++ routes/pages.js
@@ -9,11 +9,18 @@ const {
 const csrfProtection = csrf();
 router.use(csrfProtection);
 
+//GET: display abous us page
+router.get("/about-us", (req, res) => {
+  res.render("pages/about-us", {
+    pageName: "About Us",
+  });
+});
+
 //GET: display contact us page and form with csrf tokens
 router.get("/contact-us", (req, res) => {
   const successMsg = req.flash("success")[0];
   const errorMsg = req.flash("error");
-  res.render("pages/contactUs", {
+  res.render("pages/contact-us", {
     pageName: "Contact Us",
     csrfToken: req.csrfToken(),
     successMsg,

e11682b6eaff33cd44ca4eebb6a217db02c8f54d
Maryam <maria.i.135a@gmail.com>
2020-05-17 20:17:59 +0300

    Add contact us form logic using nodemailer

--- routes/pages.js
+++ routes/pages.js
@@ -1,18 +1,80 @@
 const express = require("express");
 const csrf = require("csurf");
+const nodemailer = require("nodemailer");
 const router = express.Router();
-
+const {
+  userContactUsValidationRules,
+  validateContactUs,
+} = require("../config/validator");
 const csrfProtection = csrf();
 router.use(csrfProtection);
 
 //GET: display contact us page and form with csrf tokens
 router.get("/contact-us", (req, res) => {
   const successMsg = req.flash("success")[0];
+  const errorMsg = req.flash("error");
   res.render("pages/contactUs", {
     pageName: "Contact Us",
     csrfToken: req.csrfToken(),
     successMsg,
+    errorMsg,
   });
 });
+
+//POST: handle contact us form logic using nodemailer
+router.post(
+  "/contact-us",
+  [userContactUsValidationRules(), validateContactUs],
+  (req, res) => {
+    // instantiate the SMTP server
+    const smtpTrans = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        // company's email and password
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    // email options
+    const mailOpts = {
+      from: req.body.email,
+      to: process.env.GMAIL_EMAIL,
+      subject: `Enquiry from ${req.body.name}`,
+      html: `
+      <div>
+      <h2 style="color: #478ba2; text-align:center;">Client's name: ${req.body.name}</h2>
+      <h3 style="color: #478ba2;">Client's email: (${req.body.email})<h3>
+      </div>
+      <h3 style="color: #478ba2;">Client's message: </h3>
+      <div style="font-size: 30;">
+      ${req.body.message}
+      </div>
+      `,
+    };
+
+    // send the email
+    smtpTrans.sendMail(mailOpts, (error, response) => {
+      if (error) {
+        req.flash(
+          "error",
+          "An error occured... Please check your internet connection and try again later"
+        );
+        return res.redirect("/pages/contact-us");
+      } else {
+        req.flash(
+          "success",
+          "Email sent successfully! Thanks for your inquiry."
+        );
+        return res.redirect("/pages/contact-us");
+      }
+    });
+  }
+);
 
 module.exports = router;

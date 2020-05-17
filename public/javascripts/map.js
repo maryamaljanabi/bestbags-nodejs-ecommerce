5186e5c256fdee01261053e845bbb3314d659ffb
maryamaljanabi <maryam.aljanabi135@gmail.com>
2020-05-17 22:06:01 +0300

    Add about us route and view

---
+++ public/javascripts/map.js
@@ -0,0 +1,13 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibS0xMzVhIiwiYSI6ImNrOGsyb3ZqaDBkemkzcW10emc1eXoyNngifQ.NuSNrMKqrpdm-jxvPpx0_Q";
+const lat = 48.8606;
+const lng = 2.3376;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 8,
+  center: [lng, lat],
+});
+
+const marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
